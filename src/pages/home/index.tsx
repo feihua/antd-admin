52ff1a27f0c62928132430ff7abf4b11b1b71c7e
koobe <koobe.liu@uaf.com.cn>
2023-01-17 14:55:12 +0800

    完善首页1

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -33,8 +33,73 @@ const Home: React.FC = () => {
         }
     };
 
+    const getOption2 = () => {
+        return {
+            title: {
+                text: '用户访问来源',
+                subtext: '纯属虚构',
+                x: 'center'
+            },
+            tooltip: {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            legend: {
+                orient: 'vertical',
+                left: 'left',
+                data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+            },
+            series: [
+                {
+                    name: '访问来源',
+                    type: 'pie',
+                    radius: '55%',
+                    center: ['50%', '60%'],
+                    data: [
+                        {value: 335, name: '直接访问'},
+                        {value: 310, name: '邮件营销'},
+                        {value: 234, name: '联盟广告'},
+                        {value: 135, name: '视频广告'},
+                        {value: 1548, name: '搜索引擎'}
+                    ],
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        };
+    };
+
+    const getOption3 = () => {
+        return {
+            title: {
+                text: '销量与库存'
+            },
+            tooltip: {},
+            legend: {
+                data: ['销量', '库存']
+            },
+            xAxis: {
+                data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'line',
+                data: [5, 20, 36, 10, 10, 20]
+            }, {
+                name: '库存',
+                type: 'line',
+                data: [15, 30, 46, 20, 20, 40]
+            }]
+        }
+    };
     return (
-        <div style={{height: '100vh', backgroundColor: '#f5f5f5'}}>
+        <div style={{backgroundColor: '#f5f5f5'}}>
             <Row gutter={16}>
                 <Col span={6}>
                     <Card bordered={false}>
@@ -82,14 +147,23 @@ const Home: React.FC = () => {
                     </Card>
                 </Col>
             </Row>
-            <Row style={{marginTop: 10}}>
-                <Col span={24}>
-                    {/*<Card>*/}
-                    {/*    <Button type='primary'>更新</Button>*/}
-                    {/*</Card>*/}
-                    <Card title='图一'>
-                        <ReactEcharts option={getOption()} style={{height: 300}}/>
+
+            <Row gutter={16} style={{marginTop: 10, padding: 5}}>
+                <Col span={12}>
+                    <Card bordered={false}>
+                        <ReactEcharts option={getOption()} style={{height: 280}}/>
                     </Card>
+
+                </Col>
+                <Col span={12}>
+                    <Card bordered={false}>
+                        <ReactEcharts option={getOption2()} style={{height: 280}}/>
+                    </Card>
+                </Col>
+            </Row>
+            <Row style={{marginTop: 10, backgroundColor: 'white', padding: 5}}>
+                <Col span={24}>
+                    <ReactEcharts option={getOption3()} style={{height: 280}}/>
                 </Col>
             </Row>
         </div>
