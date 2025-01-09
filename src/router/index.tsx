d0ed98b0054fe7f0cc686e6f5560212e743cd41f
liufeihua <1002219331@qq.com>
2025-01-09 15:40:39 +0800

    fix add a title to the route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -20,84 +20,99 @@ import LoginLog from "../pages/system/LoginLog";
 const routes = [
     {
         path: "/home",
+        title: "首页",
         element: <Home/>
     },
     {
         path: '/system',
+        title: "权限管理",
         element: <SystemIndex/>,
         children: [
             {
-                path: '/system/user',
+                path: 'user',
+                title: "用户管理",
                 element: <SysUser/>,
             },
             {
-                path: '/system/role',
+                path: 'role',
+                title: "角色管理",
                 element: <SysRole/>,
             },
             {
                 path: '/system/menu',
+                title: "菜单管理",
                 element: <SysMenu/>,
             },
             {
                 path: '/system/dept',
+                title: "部门管理",
                 element: <Dept/>,
             },
             {
                 path: '/system/post',
+                title: "岗位管理",
                 element: <Post/>,
             },
             {
                 path: '/system/dictType',
+                title: "字典类型",
                 element: <DictType/>,
             },
             {
                 path: '/system/dictData',
+                title: "字典数据",
                 element: <DictData dictType={""} open={true}/>,
             },
             {
                 path: '/system/notice',
+                title: "通知公告",
                 element: <Notice/>,
             },
-            {
-                path: '/system/loginLog',
-                element: <LoginLog/>,
-            },
-            {
-                path: '/system/operateLog',
-                element: <OperateLog/>,
-            },
-            {
-                path: '/system/bar',
-                element: <Bar/>,
-            },
-            {
-                path: '/system/line',
-                element: <Line/>,
-            },
-            {
-                path: '/system/pie',
-                element: <Pie/>,
-            },
-            {
-                path: '/system/center',
-                element: <Center/>,
-            },
-            {
-                path: '/system/setting',
-                element: <Setting/>,
-            },
+            // {
+            //     path: '/system/loginLog',
+            //     title: "角色管理",
+            //     element: <LoginLog/>,
+            // },
+            // {
+            //     path: '/system/operateLog',
+            //     title: "角色管理",
+            //     element: <OperateLog/>,
+            // },
+            // {
+            //     path: '/system/bar',
+            //     element: <Bar/>,
+            // },
+            // {
+            //     path: '/system/line',
+            //     element: <Line/>,
+            // },
+            // {
+            //     path: '/system/pie',
+            //     element: <Pie/>,
+            // },
+            // {
+            //     path: '/system/center',
+            //     element: <Center/>,
+            // },
+            // {
+            //     path: '/system/setting',
+            //     element: <Setting/>,
+            // },
         ],
     },
     {
         path: '/log',
+        title: "日志管理",
         element: <SystemIndex/>,
         children: [
             {
                 path: '/log/loginLog',
+                title: "登录日志",
                 element: <LoginLog/>,
             },
             {
                 path: '/log/operateLog',
+                title: "操作日志",
                 element: <OperateLog/>,
             },
 
@@ -105,6 +120,7 @@ const routes = [
     },
     {
         path: "/",
+        title: "首页",
         element: <Navigate to="/home"/>
     }
 ]
