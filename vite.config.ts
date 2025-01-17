0c572bae51d05b385c1c804fe298190e408ead3c
liufeihua <1002219331@qq.com>
2025-01-17 14:32:36 +0800

    deploy to non-root directory

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/antd',
+  base: '/antd',//deploy to non-root directory
   plugins: [react()],
   server: {
     proxy: {
