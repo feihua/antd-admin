dfc755e710ee029266f3bf49b9f47db9d5faa630
liufeihua <1002219331@qq.com>
2025-01-22 16:14:13 +0800

    update logo title

--- src/components/layout/admin/index.tsx
+++ src/components/layout/admin/index.tsx
@@ -106,7 +106,7 @@ const Admin: React.FC = () => {
                     setSelectedKeys(['/home'])
                 }}>
                     <img style={{height: 32, width: 32, margin: 16}} src={logo} alt="logo"/><h1
-                    style={{marginBottom: 0, color: "black", fontSize: 20}}>rust</h1>
+                    style={{marginBottom: 0, color: "black", fontSize: 20}}>test</h1>
                 </Link>
                 <div style={{background: 'rgb(228 229 234)', height: '1px'}}></div>
                 <Menu theme="light"
